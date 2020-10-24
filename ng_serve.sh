e80bbda53428e4a402b2512a391f35ea2e03cf7b
Ray C Horn <raychorn@gmail.com>
2020-10-24 16:16:49 -0600

    first commit.

---
+++ ng_serve.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+echo "Running..."
+cd /home/raychorn/projects/firebase-projects/crud-app1/crudapp1
+ng serve --host 10.0.0.84 --port 8088
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCWmphhcv1ZTOvgTAHh0A6dz5QCqMRk37o",
+  authDomain: "fir-project1-8dac5.firebaseapp.com",
+  databaseURL: "https://fir-project1-8dac5.firebaseio.com",
+  projectId: "fir-project1-8dac5",
+  storageBucket: "fir-project1-8dac5.appspot.com",
+  messagingSenderId: "323231490676",
+  appId: "1:323231490676:web:32a8028bd7a2bd4173a721",
+  measurementId: "G-47MDB9G3NJ"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
